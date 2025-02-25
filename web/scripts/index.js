9bab76f9c9a42b8067d944b36e0cc4c795c791d9
Temutjin2k <tamutdzhin2006@mail.ru>
2025-02-25 18:29:20 +0500

    Done with Blogs. Users can't edit or delete other users's posts, but admin can

--- web/scripts/index.js
+++ web/scripts/index.js
@@ -18,17 +18,3 @@ document.addEventListener("DOMContentLoaded", async function () {
     }
 });
 
-
-async function logout() {
-    try {
-        const response = await fetch("/api/logout", { method: "POST" });
-        if (response.ok) {
-            window.location.href = "/login";
-        } else {
-            alert(`Failed to logout.`);
-        }
-    } catch (error) {
-        console.error("Logout error:", error);
-        alert("Something went wrong!");
-    }
-}
